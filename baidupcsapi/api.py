8febe37f22f0c1318209a1890456d4f6c89e39fc
Capric <capric8416@gmail.com>
2016-03-17 22:30:02 +0800

    批量还原文件或目录方法POST数据变更
    filelist -> fidlist

--- baidupcsapi/api.py
+++ baidupcsapi/api.py
@@ -1377,7 +1377,7 @@ class PCS(BaseClass):
         """
 
         data = {
-            'filelist': json.dumps([fs_id for fs_id in fs_ids])
+            'fidlist': json.dumps(fs_ids)
         }
         url = 'http://{0}/api/recycle/restore'.format(BAIDUPAN_SERVER)
         return self._request('recycle', 'restore', data=data, **kwargs)
